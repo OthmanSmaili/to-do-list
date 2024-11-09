beca402bf72fddddde9b0bf6de5ff1046c15174f
OthmanSmaili <66753081+OthmanSmaili@users.noreply.github.com>
2024-11-09 01:41:39 -0800

    Upload files

---
+++ script.js
@@ -0,0 +1,46 @@
+var input = document.getElementById("userinput");
+var button = document.getElementById("add");
+var list = document.getElementById("todolist");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+
+    var deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.appendChild(document.createTextNode("X"));
+    deleteBtn.addEventListener("click", deleteTask);
+
+    li.appendChild(deleteBtn);
+    li.addEventListener("click", markDone);
+    list.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0){
+        createListElement();
+    }
+}
+
+function addListAfterEnter(event) {
+    if (event.key === "Enter") {
+        addListAfterClick();
+    }
+}
+
+function markDone(event) {
+    event.target.classList.toggle("done");
+}
+
+function deleteTask(event) {
+    event.stopPropagation();
+    this.parentElement.remove();
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterEnter);
